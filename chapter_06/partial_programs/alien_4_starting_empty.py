5453ed41bfa32207c8f0d99897ff9c2a40e6e673
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-08 21:52:20 +0800

    20250608 SKNEO start chapter 6
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_06/partial_programs/alien_4_starting_empty.py
+++ chapter_06/partial_programs/alien_4_starting_empty.py
@@ -4,3 +4,10 @@ alien_0['color'] = 'green'
 alien_0['points'] = 5
 
 print(alien_0)
+
+# alien_0 = {}
+
+alien_0['alien'] = 'red'
+alien_0['level'] = 25
+
+print(alien_0)
--- chapter_06/partial_programs/alien_no_points_1_using_get.py
+++ chapter_06/partial_programs/alien_no_points_1_using_get.py
@@ -1,4 +1,5 @@
 alien_0 = {'color': 'green', 'speed': 'slow'}
-
+alien_0['points'] = 5
 point_value = alien_0.get('points', 'No point value assigned.')
+
 print(point_value)
