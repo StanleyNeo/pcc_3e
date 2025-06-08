4debd1c53165f1e083cd701b42f0d11c2caa15f7
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-08 20:37:11 +0800

    # Ordinal Numbers 20250608 SKNEO 序数词

---
+++ solution_files/chapter_05/5-11_ordinal_numbers.py
@@ -0,0 +1,28 @@
+# Ordinal Numbers 20250608 SKNEO 序数词
+
+
+
+数字表 = list(range(1,21))
+
+for 数字 in 数字表:
+    if 数字 == 1:
+        print("1st 数字")
+    elif 数字 == 2:
+        print("2nd 数字")
+    elif 数字 == 3:
+        print("3rd 数字")
+    else:
+        print(f"{数字}th 数字")
+
+numbers = list(range(1, 21))
+
+print("\n-------------------------")
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
