542ae7c8fba84e49bc8a6d4e2b367c0bd441b1ea
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-08 18:25:04 +0800

    5-8 20250608 SKNEO 管理员

---
+++ solution_files/chapter_05/5-8_hello_admin.py
@@ -0,0 +1,15 @@
+usernames = ['eric', 'willie', 'admin', 'erin', 'ever']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for loggin in again!")
+
+用户名 = ['永明', '大头', '郭亮', '管理员', '婆婆', '梁总']
+
+for 用户 in 用户名:
+    if 用户 == '管理员':
+        print("您好，管理员，您想查看状态报告吗？")
+    else:
+        print(f"您好，{用户}，感谢您再次登录！")
