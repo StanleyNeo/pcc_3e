16589dca76a67e0122e15be445a1c9fedd1d4fb4
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-08 18:09:08 +0800

    5-7 20250608 SKNEO

---
+++ solution_files/chapter_05/5-7_favorite_fruits.py
@@ -0,0 +1,25 @@
+favorite_fruits = ['blueberries', 'salmonberries', 'peaches']
+
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+if 'kiwis' in favorite_fruits:
+    print("You really like kiwis!")
+if 'peaches' in favorite_fruits:
+    print("You really like peaches!")
+
+    最喜欢的_水果 = ['蓝莓', '三文鱼莓', '桃子']
+
+    if '香蕉' in 最喜欢的_水果:
+        print("你真的很喜欢香蕉！")
+    if '苹果' in  最喜欢的_水果:
+        print("你真的很喜欢苹果！")
+    if '蓝莓' in 最喜欢的_水果:
+        print("你真的很喜欢蓝莓！")
+    if '猕猴桃' in 最喜欢的_水果:
+        print("你真的很喜欢猕猴桃！")
+    if '桃子' in 最喜欢的_水果:
+        print("你真的很喜欢桃子！")
