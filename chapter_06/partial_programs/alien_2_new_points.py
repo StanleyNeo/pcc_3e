dee66cb8c18ce2eec1f7e03ba01ebca066d422c1
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-08 21:45:55 +0800

    # 外星人2新积分 20250608 SKNEO

--- chapter_06/partial_programs/alien_2_new_points.py
+++ chapter_06/partial_programs/alien_2_new_points.py
@@ -1,4 +1,11 @@
-alien_0 = {'color': 'green', 'points': 5}
+# 外星人2新积分 20250608 SKNEO
 
+外星人_0 = {'颜色': '绿色', '点数': 5}
+print(外星人_0['点数'])
+新积分 = 外星人_0['点数']
+print(f"您刚刚获得 {新积分} 积分！\n")
+
+
+alien_0 = {'color': 'green', 'points': 5}
 new_points = alien_0['points']
 print(f"You just earned {new_points} points!")
