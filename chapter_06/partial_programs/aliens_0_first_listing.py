a6f06d063cb80a89c4f580bb9f3615c149e70be8
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-09 21:43:17 +0800

    # 外星人_0_# 第一个列表 20250609 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_06/partial_programs/aliens_0_first_listing.py
+++ chapter_06/partial_programs/aliens_0_first_listing.py
@@ -1,8 +1,12 @@
+# 外星人_0_# 第一个列表 20250609 SKNEO
+
 alien_0 = {'color': 'green', 'points': 5}
 alien_1 = {'color': 'yellow', 'points': 10}
 alien_2 = {'color': 'red', 'points': 15}
 
 aliens = [alien_0, alien_1, alien_2]
 
+print(aliens)            # print all alien in one List
+
 for alien in aliens:
-  print(alien)
+  print(alien)           # print each alien
