e974d32d1111994a40fece64a56e09b76d44d59d
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-09 21:38:15 +0800

    # 外星人无点数0第一个版本 20250609 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_06/partial_programs/alien_no_points_0_first_version.py
+++ chapter_06/partial_programs/alien_no_points_0_first_version.py
@@ -1,2 +1,11 @@
-alien_0 = {'color': 'green', 'speed': 'slow'}
-print(alien_0['points'])
+# 外星人无点数0第一个版本 20250609 SKNEO
+
+alien_0 = {'color': 'green', 'speed': 'slow', 'points': '5', 'location': '200'}
+
+point_value = alien_0.get('points', 'No point value assigned.')
+location_value = alien_0.get('location', 'No point value assigned.')
+
+print(point_value)
+print(location_value)
+
+# print(alien_0['points'])
