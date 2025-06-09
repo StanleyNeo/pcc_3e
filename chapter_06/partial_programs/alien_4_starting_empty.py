56026d4c40462bf6089b31f8cb8d4362eccfd0c5
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-09 19:48:08 +0800

    # 外星人4起始空 20250609 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_06/partial_programs/alien_4_starting_empty.py
+++ chapter_06/partial_programs/alien_4_starting_empty.py
@@ -1,13 +1,15 @@
+# 外星人4起始空 20250609 SKNEO
+
+外星人_0 = {}
+
+外星人_0['颜色'] = '绿色'
+外星人_0['点数'] = 5
+
+print(外星人_0)
+
 alien_0 = {}
 
 alien_0['color'] = 'green'
 alien_0['points'] = 5
-
-print(alien_0)
-
-# alien_0 = {}
-
-alien_0['alien'] = 'red'
-alien_0['level'] = 25
 
 print(alien_0)
