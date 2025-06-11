5ee7041aa3372f06e3c5fe9deb886827c808e959
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-11 13:45:31 +0800

    # 最喜欢的语言_2_循环项目 20250611 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_06/partial_programs/favorite_languages_2_loop_items.py
+++ chapter_06/partial_programs/favorite_languages_2_loop_items.py
@@ -1,3 +1,15 @@
+# 最喜欢的语言_2_循环项目 20250611 SKNEO
+
+喜欢的编程语言 = {
+    '小美': 'Python',
+    '晓丽': 'C',
+    '大明': 'Rust',
+    '志强': 'Python',
+}
+
+for 姓名, 语言 in 喜欢的编程语言.items():
+    print(f"{姓名}最喜欢的编程语言是{语言}。")
+
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
