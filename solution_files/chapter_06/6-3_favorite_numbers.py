fde8270631482cdf3ed83954dbda321284ceccec
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-11 13:54:38 +0800

    # 6-2_喜欢的数字 20250611 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

---
+++ solution_files/chapter_06/6-3_favorite_numbers.py
@@ -0,0 +1,47 @@
+# 6-2_喜欢的数字 20250611 SKNEO
+
+喜欢的数字 = {
+    '小美': 42,
+    '小明': 23,
+    '小刚': 7,
+    '小红': 1_000_000,
+    '小丽': 0,
+}
+
+数字 = 喜欢的数字['小美']
+print(f"小美最喜欢的数字是{数字}。")
+
+数字 = 喜欢的数字['小明']
+print(f"小明最喜欢的数字是{数字}。")
+
+数字 = 喜欢的数字['小刚']
+print(f"小刚最喜欢的数字是{数字}。")
+
+数字 = 喜欢的数字['小红']
+print(f"小红最喜欢的数字是{数字}。")
+
+数字 = 喜欢的数字['小丽']
+print(f"小丽最喜欢的数字是{数字}。")
+
+favorite_numbers = {
+    'mandy': 42,
+    'micah': 23,
+    'gus': 7,
+    'hank': 1_000_000,
+    'maggie': 0,
+    }
+
+num = favorite_numbers['mandy']
+print(f"Mandy's favorite number is {num}.")
+
+num = favorite_numbers['micah']
+print(f"Micah's favorite number is {num}.")
+
+num = favorite_numbers['gus']
+print(f"Gus's favorite number is {num}.")
+
+num = favorite_numbers['hank']
+print(f"Hank's favorite number is {num}.")
+
+num = favorite_numbers['maggie']
+print(f"Maggie's favorite number is {num}.")
