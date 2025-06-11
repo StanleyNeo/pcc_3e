1c5042ea6f9b7347b783bb8def4688539d671c48
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-11 17:56:10 +0800

    7-1-1_parrot_1_while_loop
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_07/partial_programs/7-1-1_parrot_1_while_loop.py
+++ chapter_07/partial_programs/7-1-1_parrot_1_while_loop.py
@@ -10,6 +10,11 @@ while True:
 
     print(f"您说：{用户输入}")
 
+# prompt = "\nTell me something, and I will repeat it back to you:" +
+# "\nEnter 'quit' to end the program. "
+# 提示信息 = "\n请输入任意内容，我会重复您的话："
+# 提示信息 += "\n输入'退出'可结束程序。 "
+
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
