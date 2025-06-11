c8e32d093ff5ada1a841f255df1205b300410dd1
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-11 13:38:55 +0800

    # 将前3个绿色外星人改为黄色 20250611 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- chapter_06/partial_programs/favorite_languages_0_first_version.py
+++ chapter_06/partial_programs/favorite_languages_0_first_version.py
@@ -1,3 +1,15 @@
+# 喜欢的编程语言 20250611 SKNEO
+喜欢的编程语言 = {
+    '小美': 'python',
+    '晓丽': 'c',
+    '大明': 'rust',
+    '志强': 'python',
+}
+
+print("=== 开发者喜欢的编程语言 ===")
+for 名字, 语言 in 喜欢的编程语言.items():
+    print(f"{名字}最喜欢的语言是{语言}")
+
 favorite_languages = {
     'jen': 'python',
     'sarah': 'c',
