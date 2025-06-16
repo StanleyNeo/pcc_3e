46e487507c31b0a7b3dccc86ea9b47515f4a1a6e
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-16 20:12:59 +0800

    8-1_favorite_book 函数 喜欢的书籍 20250616 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- solution_files/chapter_08/8-2_favorite_book.py
+++ solution_files/chapter_08/8-2_favorite_book.py
@@ -1,3 +1,10 @@
+# 8-1_favorite_book 函数 喜欢的书籍 20250616 SKNEO
+
+def 喜欢的书籍(书名):
+    """打印关于某人最喜欢书籍的信息"""
+    print(f"《{书名}》是我最喜欢的书籍之一。")
+
+喜欢的书籍('抽象野性')
 
 def favorite_book(title):
     """Display a message about someone's favorite book."""
