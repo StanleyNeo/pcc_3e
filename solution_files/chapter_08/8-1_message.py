59e60a3b93c47f276fb9261def1f360b9b44074f
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-16 12:04:04 +0800

    # 8-1_message 显示消息 20250616 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

---
+++ solution_files/chapter_08/8-1_message.py
@@ -0,0 +1,15 @@
+# 8-1_message 显示消息 20250616 SKNEO
+
+def 显示消息():
+    """显示关于我正在学习的内容的消息"""
+    消息 = "我正在学习如何将代码存储在函数中。"
+    print(消息)
+
+显示消息()
+
+def display_message():
+    """Display a message about what I'm learning."""
+    msg = "I'm learning to store code in functions."
+    print(msg)
+
+display_message()
