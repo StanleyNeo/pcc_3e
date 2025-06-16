8277863616d758e9d39c0430eeb0c28e5f8db11e
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-16 18:19:11 +0800

    # 8-1_message 函数 显示消息 20250616 SKNEO
    Signed-off-by: SeeKwee <108121189+tuahuat999@users.noreply.github.com>

--- solution_files/chapter_08/8-1_message.py
+++ solution_files/chapter_08/8-1_message.py
@@ -1,15 +1,15 @@
-# 8-1_message 显示消息 20250616 SKNEO
+# 8-1_message 函数 显示消息 20250616 SKNEO
 
 def 显示消息():
     """显示关于我正在学习的内容的消息"""
-    消息 = "我正在学习如何将代码存储在函数中。"
+    消息 = "我正在学习如何将代码函数存储在函数中。"
     print(消息)
 
-显示消息()
+显示消息()  # call for function
 
 def display_message():
     """Display a message about what I'm learning."""
     msg = "I'm learning to store code in functions."
-    print(msg)
+    print(msg)  # call function
 
 display_message()
