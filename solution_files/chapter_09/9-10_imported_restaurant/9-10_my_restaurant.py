af2a28ff88f829f674d62b648f59483c185bb0a8
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-19 11:53:28 +0800

    9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo

--- solution_files/chapter_09/9-10_imported_restaurant/9-10_my_restaurant.py
+++
@@ -1,7 +0,0 @@
-# 9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo
-
-from restaurant import Restaurant
-
-channel_club = Restaurant('the channel club', 'steak and seafood')
-channel_club.describe_restaurant()
-channel_club.open_restaurant()
--- solution_files/chapter_09/9-10_imported_restaurant/我的餐厅.py
+++ solution_files/chapter_09/9-10_imported_restaurant/我的餐厅.py
@@ -1,6 +1,35 @@
 # 9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo
 """A class representing a restaurant."""
 
+"""表示餐厅的类"""
+
+class 餐厅:
+    """表示餐厅的类"""
+
+    def __init__(self, 名称, 菜系类型):
+        """初始化餐厅属性"""
+        self.名称 = 名称.title()
+        self.菜系类型 = 菜系类型
+        self.已服务人数 = 0
+
+    def 描述餐厅(self):
+        """显示餐厅摘要信息"""
+        信息 = f"{self.名称} 提供美味的 {self.菜系类型}。"
+        print(f"\n{信息}")
+
+    def 开业状态(self):
+        """显示餐厅正在营业的信息"""
+        信息 = f"{self.名称} 正在营业，欢迎光临！"
+        print(f"\n{信息}")
+
+    def 设置服务人数(self, 人数):
+        """设置已服务的顾客人数"""
+        self.已服务人数 = 人数
+
+    def 增加服务人数(self, 新增人数):
+        """增加已服务的顾客人数"""
+        self.已服务人数 += 新增人数
+
 class Restaurant:
     """A class representing a restaurant."""
 
