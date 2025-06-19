dd299be9eb27bfba43d5896dc4b91cd3d77726b7
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-19 10:18:16 +0800

    9-16_login_attempt 类和对象 登录尝试 20250618 Stanley Neo

--- solution_files/chapter_09/9-16_login_attempts.py
+++ solution_files/chapter_09/9-16_login_attempts.py
@@ -1,3 +1,10 @@
+# 9-16_login_attempt 类和对象 登录尝试 20250618 Stanley Neo
+'''
+9-16. Python Module of the Week: One excellent resource for exploring the Python standard library 
+is a site called Python Module of the Week. Go to https://pymotw.com and look at the table of contents. 
+Find a module that looks interesting to you and read about it, perhaps starting with the random module
+'''
+
 class User:
     """Represent a simple user profile."""
 
