fc79676f7b32a32695b1841a243de1ee7dd2d07c
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-19 15:38:10 +0800

    9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo

--- solution_files/chapter_09/9-10_imported_restaurant/类和对象9-10_我的餐厅.py
+++ solution_files/chapter_09/9-10_imported_restaurant/类和对象9-10_我的餐厅.py
@@ -1,14 +1,12 @@
 # 9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo
 
-# 9-10_my_restaurant 类和对象 我的餐厅 20250619 Stanley Neo
-
-from 我的餐厅 import 餐厅
+from 我的餐厅模块 import 餐厅
 
 海鲜牛排馆 = 餐厅('频道俱乐部', '牛排和海鲜')
 海鲜牛排馆.描述餐厅()
 海鲜牛排馆.开业状态()
 
-from 我的餐厅 import Restaurant
+from 我的餐厅模块 import Restaurant
 
 channel_club = Restaurant('the channel club', 'steak and seafood')
 channel_club.describe_restaurant()
