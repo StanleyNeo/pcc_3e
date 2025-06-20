a334fd4d1063f7963609c83e1acfd8177c7f3c79
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-21 00:08:27 +0800

    # 10-2_update writing to a file 20250620

--- solution_files/chapter_10/10-2_learning_c.py
+++ solution_files/chapter_10/10-2_learning_c.py
@@ -7,3 +7,8 @@ lines = contents.splitlines()
 for line in lines:
     line = line.replace('Python', 'C')
     print(line)
+
+# .replace()
+message = "I really like dogs."
+message = message.replace('dog', 'cat')
+print(message)
