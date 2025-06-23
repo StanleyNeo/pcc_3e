3797e2198bfc86141a79de707844f0c6a00e824e
Stanley Neo <stanley88@live.com>
2025-06-23 21:12:30 +0800

    Add 10-3_simple_code

---
+++ solution_files/chapter_10/10-3_simpler_code_file_reader.py
@@ -0,0 +1,31 @@
+# 10-1_file_reader 文件和异常 读取文件内容 20250620 Stanley Neo
+
+from pathlib import Path
+
+# 读取文件内容并打印
+print("读取文件内容")
+文件路径 = Path('pi_digits.txt')
+文件内容 = 文件路径.read_text()
+print(文件内容)
+
+# 再次读取文件并按行处理
+print("再次读取文件并按行处理")
+文件路径 = Path('pi_digits.txt')
+文件内容 = 文件路径.read_text()
+
+行列表 = 文件内容.splitlines()  # 移除空行和空白字符
+for 行 in 行列表:
+    print(行)
+
+# from pathlib import Path
+print(" \n<English Code>")
+path = Path('pi_digits.txt')
+contents = path.read_text()
+print(contents)
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+lines = contents.splitlines()  # remove blank line strips, any whitespace characters
+for line in lines:
+    print(line)
