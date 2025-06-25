b7cec7214f9ae94ad181d7eec500c08433017bf0
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-25 23:01:54 +0800

    11-1_test_cities 测试代码 测试城市国家 20250625 Stanley Neo

--- solution_files/chapter_11/11-1_city_country/city_functions.py
+++ solution_files/chapter_11/11-1_city_country/city_functions.py
@@ -1,4 +1,10 @@
-# 11-1_city_functions 测试代码 城市国家 20250625 Stanley Neo
+# 11-1_city_functions 测试代码 城市和国家 20250625 Stanley Neo
+
+"""用于处理城市和国家相关功能的函数集合"""
+
+def 城市国家(城市名, 国家名):
+    """返回格式为'城市名, 国家名'的字符串"""
+    return f"{城市名.title()}, {国家名.title()}"
 
 """A collection of functions for working with cities."""
 
--- solution_files/chapter_11/11-1_city_country/test_cities.py
+++ solution_files/chapter_11/11-1_city_country/test_cities.py
@@ -1,3 +1,28 @@
+# 11-1_test_cities 测试代码 测试城市国家 20250625 Stanley Neo
+
+from city_functions import 城市国家
+
+
+def 测试城市国家函数():
+    """测试简单的城市国家组合是否能正常工作"""
+    # 测试用例1：普通城市国家组合
+    圣地亚哥_智利 = 城市国家('santiago', 'chile')
+    assert 圣地亚哥_智利 == 'Santiago, Chile'
+
+    # 测试用例2：包含空格的地区名
+    金文泰_新加坡 = 城市国家('clementi', 'singapore')
+    assert 金文泰_新加坡 == 'Clementi, Singapore'
+
+    # 测试用例3：多单词城市名
+    香港_中国 = 城市国家('hong kong', 'china')
+    assert 香港_中国 == 'Hong Kong, China'
+
+    print("所有测试用例通过！")
+
+
+# 运行测试
+测试城市国家函数()
+
 from city_functions import city_country
 
 def test_city_country():
