f203789fccd8fb07382a22c60b243a06369ffb5c
SeeKwee <108121189+tuahuat999@users.noreply.github.com>
2025-06-26 16:16:58 +0800

    add fig.write_html('dice_visual_d6d10.html')

--- chapter_15/rolling_dice/dice_visual_d6d10.py
+++ chapter_15/rolling_dice/dice_visual_d6d10.py
@@ -28,5 +28,7 @@ fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 
 # Further customize chart.
 fig.update_layout(xaxis_dtick=1)
+
+fig.write_html('dice_visual_d6d10.html')
               
 fig.show()
